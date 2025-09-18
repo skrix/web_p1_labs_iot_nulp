80c553e1c2b8ecdc4a3df9a69a799da8a0c8039d
Andrew <andrew.shyika@gmail.com>
2025-09-19 00:08:12 +0300

    fix remove & edit btns

--- lab_3_4_5/js/app.js
+++ lab_3_4_5/js/app.js
@@ -1,18 +1,4 @@
-import { renderBooks, handleSearch, handleClear, handleSortToggle, handleCalculate } from './pages/index.js';
 import Store from './store.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const store = new Store();
-  const searchInput = document.getElementById('js-search-input');
-  const searchButton = document.getElementById('js-search-btn');
-  const clearButton = document.getElementById('js-clear-btn');
-  const sortToggle = document.getElementById('js-sort-toggle');
-  const calculateButton = document.getElementById('js-calculate-btn');
+window.store = new Store();
 
-  renderBooks(store.books);
-
-  searchButton.addEventListener('click', handleSearch(store, searchInput, sortToggle));
-  clearButton.addEventListener('click', handleClear(store, searchInput, sortToggle));
-  sortToggle.addEventListener('change', handleSortToggle(store, sortToggle));
-  calculateButton.addEventListener('click', handleCalculate(store));
-});
