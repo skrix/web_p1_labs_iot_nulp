4cb544974c81de8649d36e383bc7285630607a68
Andrew <andrew.shyika@gmail.com>
2025-09-19 00:08:12 +0300

    add book cards rendering

--- lab_3_4_5/js/app.js
+++ lab_3_4_5/js/app.js
@@ -1,7 +1,25 @@
 import Store from './store.js';
+import cardTemplate from '../components/card.html?raw';
 
-document.addEventListener('DOMContentLoaded', () => {
+function renderBookTemplate(book, template) {
+  return template
+    .replace(/{{image_url}}/g, book.image_url)
+    .replace(/{{title}}/g, book.title)
+    .replace(/{{author}}/g, book.author)
+    .replace(/{{description}}/g, book.description)
+    .replace(/{{genre}}/g, book.genre)
+    .replace(/{{isbn}}/g, book.isbn)
+    .replace(/{{price}}/g, book.price)
+    .replace(/{{edit_url}}/g, `../pages/edit.html?id=${book.id}`)
+    .replace(/{{remove_url}}/g, `#remove-${book.id}`);
+}
+
+function renderBooks(books, template) {
+  const booksGrid = document.getElementById('books-grid');
+  booksGrid.innerHTML = books.map(book => renderBookTemplate(book, template)).join('');
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
   const store = new Store();
-
-  console.log(store.books);
+  renderBooks(store.books, cardTemplate);
 });
--- lab_3_4_5/js/data.js
+++ lab_3_4_5/js/data.js
@@ -8,7 +8,7 @@ export default {
       price: 500,
       genre: 'science_fiction',
       isbn: '978-617-679-720-3',
-      image_url: 'https://starylev.com.ua/storage/books/cover/tanets-nedoumka.jpg'
+      image_url: 'https://book-ye.com.ua/media/catalog/product/cache/274ba2bb1664e69238223826e1132b42/5/2/52e81d41-de9d-11e9-8121-000c29ae1566_04a1a32a-7b94-11ed-8177-0050568ef5e6_19.jpg'
     },
     {
       id: 2,
@@ -18,7 +18,7 @@ export default {
       price: 300,
       genre: 'fantasy',
       isbn: '978-966-2961-60-7',
-      image_url: 'https://book-ye.com.ua/upload/resize_cache/iblock/5e0/podorozh-na-pup-zemli.jpg'
+      image_url: 'https://book-ye.com.ua/media/catalog/product/cache/274ba2bb1664e69238223826e1132b42/0/5/055b43fb-8ca0-11e6-80c0-000c29ae1566_ae2e5f4f-8ef0-11e7-80cf-000c29ae1566.jpg'
     },
     {
       id: 3,
@@ -28,7 +28,7 @@ export default {
       price: 165,
       genre: 'self_help',
       isbn: '978-617-7388-13-4',
-      image_url: 'https://nashformat.ua/image/catalog/products/709026.jpg'
+      image_url: 'https://book-ye.com.ua/media/catalog/product/cache/274ba2bb1664e69238223826e1132b42/1/a/1a3f39d4-1df9-11e7-80c5-000c29ae1566_e0d3bc6b-94f9-11ea-8136-000c29ae1566.jpg'
     },
     {
       id: 4,
@@ -38,7 +38,7 @@ export default {
       price: 350,
       genre: 'science_fiction',
       isbn: '978-617-664-252-2',
-      image_url: 'https://book-ye.com.ua/upload/resize_cache/iblock/cc5/chas-nayprostiша-rich.jpg'
+      image_url: 'https://book-ye.com.ua/media/catalog/product/cache/274ba2bb1664e69238223826e1132b42/5/9/59d79aed-c624-11e8-810b-000c29ae1566_a17ed463-91b0-11ed-8178-0050568ef5e6.jpg'
     },
     {
       id: 5,
@@ -48,7 +48,7 @@ export default {
       price: 360,
       genre: 'science_fiction',
       isbn: '978-617-664-253-9',
-      image_url: 'https://book-ye.com.ua/upload/resize_cache/iblock/f6e/tranzitna-stantsiya.jpg'
+      image_url: 'https://book-ye.com.ua/media/catalog/product/cache/274ba2bb1664e69238223826e1132b42/e/1/e1511da8-db7c-11e8-810d-000c29ae1566_cea16ee4-91ae-11ed-8178-0050568ef5e6.jpg'
     },
     {
       id: 6,
@@ -58,7 +58,7 @@ export default {
       price: 741,
       genre: 'non_fiction',
       isbn: '978-617-7107-71-1',
-      image_url: 'https://nashformat.ua/storage/books/cover/bronovanyj-rozum.jpg'
+      image_url: 'https://nashformat.ua/files/products/bronyovanyi-rozum-cover-book-a5-final-ua_page-0001.800x800.jpg'
     }
   ]
 };
