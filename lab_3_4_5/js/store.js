5be4d5ec8e7891b09ced41755a45f720c1ec213e
Andrew <andrew.shyika@gmail.com>
2025-09-19 00:08:12 +0300

    add store with books

---
+++ lab_3_4_5/js/store.js
@@ -0,0 +1,64 @@
+export default {
+  books: [
+    {
+      id: 1,
+      title: 'Танець недоумка',
+      author: 'Ілларіон Павлюк',
+      description: 'Науково-фантастичний роман із психологічним підтекстом про таємниці людської свідомості та космосу.',
+      price: 500,
+      genre: 'science_fiction',
+      isbn: '978-617-679-720-3',
+      image_url: 'https://starylev.com.ua/storage/books/cover/tanets-nedoumka.jpg'
+    },
+    {
+      id: 2,
+      title: 'Подорож на Пуп Землі',
+      author: 'Макс Кідрук',
+      description: 'Пригодницько-фантастичний роман про експедицію на острів Пасхи з поєднанням наукових фактів і містики.',
+      price: 300,
+      genre: 'fantasy',
+      isbn: '978-966-2961-60-7',
+      image_url: 'https://book-ye.com.ua/upload/resize_cache/iblock/5e0/podorozh-na-pup-zemli.jpg'
+    },
+    {
+      id: 3,
+      title: 'Напролом. Мистецтво перетворювати перешкоди на перемоги',
+      author: 'Раян Голідей',
+      description: 'Книга-нон-фікшн про стоїцизм, як перешкоди можуть стати можливістю для розвитку і досягнення перемоги.',
+      price: 165,
+      genre: 'self_help',
+      isbn: '978-617-7388-13-4',
+      image_url: 'https://nashformat.ua/image/catalog/products/709026.jpg'
+    },
+    {
+      id: 4,
+      title: 'Час — найпростіша річ',
+      author: 'Кліффорд Сімак',
+      description: 'Фантастичний роман про телепатів, що відкривають нові світи і ведуть боротьбу за свободу.',
+      price: 350,
+      genre: 'science_fiction',
+      isbn: '978-617-664-252-2',
+      image_url: 'https://book-ye.com.ua/upload/resize_cache/iblock/cc5/chas-nayprostiша-rich.jpg'
+    },
+    {
+      id: 5,
+      title: 'Транзитна станція',
+      author: 'Кліффорд Сімак',
+      description: 'Роман про людство як транзитну ланку у розвитку розумного життя у Всесвіті.',
+      price: 360,
+      genre: 'science_fiction',
+      isbn: '978-617-664-253-9',
+      image_url: 'https://book-ye.com.ua/upload/resize_cache/iblock/f6e/tranzitna-stantsiya.jpg'
+    },
+    {
+      id: 6,
+      title: 'Броньований Розум',
+      author: 'Констянтин Ульянов',
+      description: 'Нон-фікшн книга про стресостійкість, психологічну підготовку та витривалість у бойових умовах.',
+      price: 741,
+      genre: 'non_fiction',
+      isbn: '978-617-7107-71-1',
+      image_url: 'https://nashformat.ua/storage/books/cover/bronovanyj-rozum.jpg'
+    }
+  ]
+};
