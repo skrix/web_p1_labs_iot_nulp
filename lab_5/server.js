edd338e6b213b49a184ce63e1f4c55465ff7a0d2
Andrew <andrew.shyika@gmail.com>
2025-10-20 23:20:23 +0300

    add root hello

--- lab_5/server.js
+++ lab_5/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.json({ message: "Hello world!" });
 })
 
