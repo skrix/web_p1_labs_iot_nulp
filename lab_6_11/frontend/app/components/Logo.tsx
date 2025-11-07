9ae84a8d7a52fd06e5b936ddb7bce7d6abd50f66
Andrew <andrew.shyika@gmail.com>
2025-11-07 20:16:04 +0200

    add link to logo

--- lab_6_11/frontend/app/components/Logo.tsx
+++ lab_6_11/frontend/app/components/Logo.tsx
@@ -1,8 +1,9 @@
+import { Link } from "react-router";
 import eyeLogo from "../assets/eye_logo.svg";
 
 export function Logo() {
   return (
-    <div className="px-4 py-2 flex items-center">
+    <Link to="/" className="px-4 py-2 flex items-center hover:opacity-80 transition-opacity">
       <div className="w-16 h-8 overflow-hidden flex items-center justify-center">
         <img
           src={eyeLogo}
@@ -10,6 +11,6 @@ export function Logo() {
           className="h-full w-full object-cover scale-200"
         />
       </div>
-    </div>
+    </Link>
   );
 }
