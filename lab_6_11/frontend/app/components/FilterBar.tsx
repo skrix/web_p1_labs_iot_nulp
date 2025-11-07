9ba0d241c793ed854975e654efbb424db41edcd6
Andrew <andrew.shyika@gmail.com>
2025-11-07 20:20:58 +0200

    move from SSR to CSR

--- lab_6_11/frontend/app/components/FilterBar.tsx
+++ lab_6_11/frontend/app/components/FilterBar.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useState, useEffect } from "react";
 import { SearchBar } from "./SearchBar";
 import { FilterDropdown } from "./FilterDropdown";
--- lab_6_11/frontend/app/components/ProductDetails.tsx
+++ lab_6_11/frontend/app/components/ProductDetails.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useState, useEffect } from "react";
 import { Spinner } from "./Spinner";
 import type { Product } from "../services/products.api";
